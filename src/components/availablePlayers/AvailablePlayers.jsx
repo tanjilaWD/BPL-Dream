8a0a21f8a23ab1ddec4318b3e32533d8857dc2ca
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-03-25 23:00:47 +0600

    selected

--- src/components/availablePlayers/AvailablePlayers.jsx
+++ src/components/availablePlayers/AvailablePlayers.jsx
@@ -5,13 +5,13 @@ import player from './../player/Player';
 import Card from '../ui/Card';
 
 const AvailablePlayers = ({players}) => {
-    console.log(players);
+  
     return (
     <div>
      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10'>
     {
        players.map(player => {
-            console.log(player, 'player');
+          
          return (
            <Card player={player}></Card>
          ) ;
--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -1,14 +1,24 @@
-import React, { use } from 'react';
+import { use, useState } from 'react';
 import AvailablePlayers from '../availablePlayers/AvailablePlayers';
+import SelectedPlayers from '../selectedPlayers/SelectedPlayers';
 
 const player = ({playersPromise}) => {
-    console.log(playersPromise);
     const players = use(playersPromise);
-    console.log(players);
+    const [selectedType, setSelectedType] = useState('available');
+    console.log(selectedType, 'selectedType');
     return (
-        <div className='container mx-auto'>
-           player : {players.length}
-           <AvailablePlayers players={players}></AvailablePlayers>
+        <div className='container mx-auto my-15'>
+          
+          <div className='flex justify-between items-center'>
+            {selectedType === 'available' ? (<h2 className='font-bold text-3xl'>Available Players</h2>) : (<h2 className='font-bold text-3xl'>Selected Players 2/10</h2>)}
+            <div>
+                <button onClick={()=> setSelectedType('available')} className= {`btn ${selectedType === 'available' ? 'bg-[#E7FE29]' : ''} mr-4 rounded-r-none rounded-l-xl `}>Available</button>
+
+              <button onClick={()=> setSelectedType('selected')} className={` btn ${selectedType === 'selected' ? 'bg-[#E7FE29]' : ''} rounded-l-none rounded-r-xl `}>Selected (0)</button>  
+            </div>
+          </div>
+
+          { selectedType === 'available' ? ( <AvailablePlayers players={players}></AvailablePlayers>) : ( <SelectedPlayers></SelectedPlayers> )}
         </div>
     );
 };
