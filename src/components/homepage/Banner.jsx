d65d1fb99e1e4a39f29453b39b1eb6c689be73c5
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-03-27 22:46:18 +0600

    banner section

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import bannerImg from '../../assets/banner-main.png';
 
 const Banner = () => {
     return (
-        <div className='min-h-[60vh] bg-linear-to-r from-purple-500 to-purple-300 container mx-auto'>
-          Banner  
+        <div className='min-h-[60vh] container mx-auto my-10 bg-green-700 rounded-3xl p-10'>
+           <div className='text-center space-y-4'>
+            <div className='flex justify-center items-center'>
+                 <img src={bannerImg} alt="Banner Image"/>
+            </div>
+          
+            <h2 className='text-4xl font-bold text-white'>Assemble Your Ultimate Dream 11 Cricket Team</h2>
+            <p className='text-2xl text-[#FFFFFF]'>Beyond Boundaries Beyond Limits</p>
+            <button className="px-6 py-3 text-black font-semibold rounded-lg 
+                         bg-[#E7FE29]">
+            Claim Free Credit
+           </button>
+           </div>
         </div>
     );
 };
--- src/components/navber/Navber.jsx
+++ src/components/navber/Navber.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import dollerImg from '../../assets/dollar.png'
+import dollerImg from '../../assets/dollar.png';
+import logo from '../../assets/logo.png';
+
 
 const Navber = ({coin}) => {
     return (
-        <div className="navbar bg-base-100 shadow-sm container mx-auto">
+        <div className="navbar bg-base-100 shadow-sm container mx-auto my-5">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl"><img src={logo} alt="Logo" /></a>
   </div>
   <div className="flex-none">
     <button className=" flex justify-between items-center gap-2 font-bold text-2xl">
